bc72e6795d2bf6eaa2a3e3a25320bb4abf0b51ae
KorotaevT <korotaevtimofey@gmail.com>
2024-02-23 07:30:18 +0300

    initial commit

---
+++ src/main/kotlin/ru/vsu/cs/tp/freelanceExchangeService/FreelanceExchangeServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.vsu.cs.tp.freelanceExchangeService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreelanceExchangeServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<FreelanceExchangeServiceApplication>(*args)
+}
