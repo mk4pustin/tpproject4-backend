4b53e2bdba9ca7a8d2899b28ddd5058454cd5017
KorotaevT <korotaevtimofey@gmail.com>
2024-02-26 23:31:13 +0300

    fixed initial commit

--- src/test/kotlin/ru/vsu/cs/tp/freelanceFinderServer/FreelanceFinderServerApplicationTests.kt
+++ src/test/kotlin/ru/vsu/cs/tp/freelanceFinderServer/FreelanceFinderServerApplicationTests.kt
@@ -1,10 +1,10 @@
-package ru.vsu.cs.tp.freelanceExchangeService
+package ru.vsu.cs.tp.freelanceFinderServer
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class FreelanceExchangeServiceApplicationTests {
+class FreelanceFinderServerApplicationTests {
 
 	@Test
 	fun contextLoads() {
