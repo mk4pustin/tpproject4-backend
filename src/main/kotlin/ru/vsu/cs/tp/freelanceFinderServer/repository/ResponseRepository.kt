82085ff0de7bf41d7a3abcf7e3532087ca5b32a4
KorotaevT <korotaevtimofey@gmail.com>
2024-06-20 01:04:17 +0300

    add exceptions

--- src/main/kotlin/ru/vsu/cs/tp/freelanceFinderServer/repository/ResponseRepository.kt
+++ src/main/kotlin/ru/vsu/cs/tp/freelanceFinderServer/repository/ResponseRepository.kt
@@ -1,6 +1,12 @@
 package ru.vsu.cs.tp.freelanceFinderServer.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import ru.vsu.cs.tp.freelanceFinderServer.model.Order
 import ru.vsu.cs.tp.freelanceFinderServer.model.Response
+import ru.vsu.cs.tp.freelanceFinderServer.model.User
 
-interface ResponseRepository : JpaRepository<Response, Long>
+interface ResponseRepository : JpaRepository<Response, Long> {
+
+    fun existsByUserAndOrder(user: User, order: Order): Boolean
+
+}
