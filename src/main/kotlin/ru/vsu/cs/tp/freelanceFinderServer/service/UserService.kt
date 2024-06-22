2d4ce95aa2237af7c438cf3778175aef05db3379
KorotaevT <korotaevtimofey@gmail.com>
2024-06-22 15:53:39 +0300

    fix update

--- src/main/kotlin/ru/vsu/cs/tp/freelanceFinderServer/service/UserService.kt
+++ src/main/kotlin/ru/vsu/cs/tp/freelanceFinderServer/service/UserService.kt
@@ -1,6 +1,7 @@
 package ru.vsu.cs.tp.freelanceFinderServer.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import ru.vsu.cs.tp.freelanceFinderServer.dto.ClaimDTO
 import ru.vsu.cs.tp.freelanceFinderServer.dto.UserDTO
@@ -18,9 +19,10 @@ class UserService @Autowired constructor(
 
     private val userRepository: UserRepository,
     private val scopeRepository: ScopeRepository,
-    private val jwtService: JwtService
+    private val jwtService: JwtService,
+    private val passwordEncoder: PasswordEncoder,
 
-) {
+    ) {
 
     fun getFreelancers(): List<User> {
         return userRepository.findAllByRoleName("Freelancer")
@@ -35,7 +37,7 @@ class UserService @Autowired constructor(
 
         userDto.username?.takeIf { it.isNotEmpty() }?.let { user.username = it }
         userDto.email?.takeIf { it.isNotEmpty() }?.let { user.email = it }
-        userDto.password?.takeIf { it.isNotEmpty() }?.let { user.password = it }
+        userDto.password?.takeIf { it.isNotEmpty() }?.let { user.password = passwordEncoder.encode(it) }
         userDto.aboutMe?.takeIf { it.isNotEmpty() }?.let { user.aboutMe = it }
         userDto.contact?.takeIf { it.isNotEmpty() }?.let { user.contact = it }
         userDto.skills?.takeIf { it.isNotEmpty() }?.let { user.skills = it }
